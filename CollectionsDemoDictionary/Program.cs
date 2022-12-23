7dd5643f3e3cfce04b6b812bf0751e4b0f23a63d
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-23 18:07:45 +0530

    [Prashant]: Dictionary collections demo

---
+++ CollectionsDemoDictionary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace CollectionsDemoDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C# Collections");
+            doDictionaryDemo();
+        }
+        public static void doDictionaryDemo()
+        {
+            Console.WriteLine("\nIn doDictionarytDemo");
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+            dictionary.Add(100, "Amit");
+            dictionary.Add(101, "Ayush");
+            dictionary.Add(102, "Vijay");
+            Console.WriteLine("Access value using Key(Key = 100): " + dictionary[100]);
+            Console.WriteLine("\nIterating Dictionary");
+            foreach (var element in dictionary)
+            {
+                Console.WriteLine("Key =" + element.Key + " &  value =" + element.Value);
+            }
+
+                
+        }
+    }
+}
+    
+
+ 
+
