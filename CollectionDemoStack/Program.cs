85d4c14e7491f71a093e6c11bbf138ef5ac8d41d
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-23 18:06:05 +0530

    [Prashant]: Stack collectons demo

---
+++ CollectionDemoStack/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace CollectionDemoStack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C# Collections");
+            doStackDemo();
+        }
+        public static void doStackDemo()
+        {
+            Console.WriteLine("\nIn doStackDemo");
+            Stack<string> stack = new Stack<string>();
+            stack.Push("Ayush");
+            stack.Push("Ravi");
+            stack.Push("Amit");
+            stack.Push("Garima");
+            
+            string pop = stack.Pop();
+            foreach (var element in stack)
+            {
+                Console.WriteLine("elements");
+            }
+            Console.WriteLine("Pooped element: " + pop);
+        }
+    }
+}
