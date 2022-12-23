991f04de5507add89e418ee81dec2170519cfcfb
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-23 18:04:01 +0530

    [Prashant]:LIst collection demo

---
+++ CollectionsDemoList/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+namespace CollectionsDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C# Collections");
+            doListDemo();
+            
+        }
+        private static void doListDemo()
+        {
+            Console.WriteLine("\nIn doListDemo");
+            List<string> list = new List<string>();
+            list.Add("Ravi");
+            list.Add("Vijay");
+            list.Add("Ravi");
+            list.Add("Ajay");
+
+            foreach(var element in list)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
